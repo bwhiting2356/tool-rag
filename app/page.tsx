7d5f71e813516c97237b56221ad04f06ee578201
Brendan Whiting <brendanwhiting@Brendans-MacBook-Air.local>
2024-06-13 12:49:11 -0700

    adding hotjar

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,19 @@ import Explanation from './components/Explanation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Chat from './components/Chat';
 import { ToolRagProvider } from './state/context';
+import { useEffect } from 'react';
+import Hotjar from '@hotjar/browser';
 
 const queryClient = new QueryClient();
 
+const siteId = process.env.NEXT_PUBLIC_HOTJAR_SITE_ID;
+const hotjarVersion = 6;
+
 export default function Home() {
+    useEffect(() => {
+        Hotjar.init(siteId as any, hotjarVersion);
+    }, []);
+
     return (
         <main className="flex h-screen flex items-center justify-between">
             <QueryClientProvider client={queryClient}>
